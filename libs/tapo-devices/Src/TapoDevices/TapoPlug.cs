9454308aac1a889c59901fecdba3f81dd289f76b
Leon Bogaert <leon@omniboost.io>
2025-09-12 16:36:13 +0200

    asked copilot to add tapo-devices as a dependency

---
+++ libs/tapo-devices/Src/TapoDevices/TapoPlug.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TapoDevices
+{
+    /// <summary>
+    /// Represents connection to Tapo Smart Wi-Fi Socket P100/P110/P300 device.
+    /// </summary>
+    /// <remarks>SMART.TAPOPLUG</remarks>
+    public class TapoPlug : TapoDevice
+    {
+        public TapoPlug(
+            string ipAddress,
+            string username,
+            string password) : base(ipAddress, username, password)
+        {
+
+        }
+
+        public TapoPlug(
+            string ipAddress,
+            string username,
+            string password,
+            TimeSpan defaultTimeout) : base(ipAddress, username, password, defaultTimeout)
+        {
+
+        }
+
+        public async Task<GetDeviceUsage.ResultPlug> GetDeviceUsageAsync()
+        {
+            var request = GetDeviceUsage.CreateRequest();
+            return await PostSecuredAsync<TapoRequest<GetDeviceUsage.Params>, GetDeviceUsage.ResultPlug>(request);
+        }
+
+        public async Task<GetEnergyUsage.ResultPlug> GetEnergyUsageAsync()
+        {
+            var request = GetEnergyUsage.CreateRequest();
+            return await PostSecuredAsync<TapoRequest<GetEnergyUsage.Params>, GetEnergyUsage.ResultPlug>(request);
+        }
+
+        public async Task<GetLedInfo.ResultPlug> GetLedInfoAsync()
+        {
+            var request = GetLedInfo.CreateRequest();
+            return await PostSecuredAsync<TapoRequest<GetLedInfo.Params>, GetLedInfo.ResultPlug>(request);
+        }
+    }
+}
